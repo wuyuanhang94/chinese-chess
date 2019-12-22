8df1f6f32aa297aeb32bbd253db8b69d4db420d8
wuyuanhang94 <764591983@qq.com>
2019-12-22 21:53:08 +0800

    simple moving of stones complete

--- Classes/SceneGame.cpp
+++ Classes/SceneGame.cpp
@@ -52,5 +52,135 @@ bool SceneGame::init(bool red)
 
         _s[i]->setPosition(ccp(_s[i]->getX()*_d, _s[i]->getY()*_d) + _stoneOffset);
     }
+    _selectid = -1;
+    _selectSprite = CCSprite::create("selected.png");
+    this->addChild(_selectSprite);
+    _selectSprite->setVisible(false);
+    _selectSprite->setScale(.8f);
+    _redTurn = true;
+
+    //enable touching
+    setTouchEnabled(true);
+    setTouchMode(kCCTouchesOneByOne);
+
+    //加悔棋按钮
+    CCMenu* menu = CCMenu::create();
+    CCMenuItem* item = CCMenuItemImage::create("regret.jpg","regret.jgp",this,menu_selector(SceneGame::back));
+    menu->addChild(item);
+    menu->setPosition(ccp(winSize.width/2+200, winSize.height/2));
+    addChild(menu);
+    //创建悔棋array
+    _steps = CCArray::create();
+    _steps->retain();
+
+    //干死老将后换边
+    _redSide = red;
+
     return true;
+}
+
+void SceneGame::back(CCObject *)
+{
+    if(_steps->count() == 0) return;
+
+    Step* step = (Step*)_steps->lastObject();
+    _steps->removeLastObject();
+    _s[step->_moveid]->setX(step->_xFrom);
+    _s[step->_moveid]->setY(step->_yFrom);
+    _s[step->_moveid]->setPosition(getStonePos(step->_xFrom, step->_yFrom));
+
+    if(step->_killid != -1)
+    {
+        _s[step->_killid]->setVisible(true);
+        _s[step->_killid]->setDead(false);
+    }
+
+    _redTurn = !_redTurn;
+}
+
+void SceneGame::setSelectID(int id)
+{
+    if(id == -1 || _s[id]->getRed() != _redTurn) return;
+    _selectid = id;
+
+    _selectSprite->setVisible(true);
+    _selectSprite->setPosition(_s[_selectid]->getPosition());
+}
+
+void SceneGame::moveStone(int moveid, int killid, int x, int y)
+{
+    //if(killid == -1) return;//这么写是有bug的 只有当killid有子时才能move过去 显然不对
+    if(killid != -1 && _s[moveid]->getRed() == _s[killid]->getRed())
+    {
+        setSelectID(killid);
+        return;
+    }
+
+    Step* step = Step::create(moveid,killid,_s[moveid]->getX(),_s[moveid]->getY(),x,y);
+    _steps->addObject(step);
+
+    if(killid != -1)
+    {
+        _s[killid]->setDead(true);
+        _s[killid]->setVisible(false);
+
+        if(_s[killid]->getType() == Stone::JIANG)
+        {
+            CCDirector::sharedDirector()->replaceScene(SceneGame::scene(!_redSide));
+        }
+    }
+
+    //moveid不受killid影响 (x,y)应该在上一个判断中判断过为合法坐标 moveid可以移动
+    _s[moveid]->setX(x);
+    _s[moveid]->setY(y);
+    _s[moveid]->setPosition(getStonePos(x,y));
+
+    _selectid = -1;
+    _selectSprite->setVisible(false);
+    _redTurn = !_redTurn;
+}
+
+CCPoint SceneGame::getStonePos(int x, int y)
+{
+    return ccp(x*_d, y*_d) + this->_stoneOffset;
+}
+
+bool SceneGame::ccTouchBegan(CCTouch *pTouch, CCEvent *)
+{
+    CCPoint ptInWin = pTouch->getLocation();
+    int x,y;
+    //找到点击对应的棋盘坐标
+    if(!getClickPos(ptInWin, x, y))
+        return false;
+
+    int clickid = getStone(x,y);
+    if(_selectid == -1)
+        setSelectID(clickid);
+    else
+        moveStone(_selectid, clickid, x, y);
+    return true;
+}
+
+int SceneGame::getStone(int x, int y)
+{
+    Stone* s;
+    for(int i=0;i<32;i++)
+    {
+        s = _s[i];
+        if(s->getX() == x && s->getY() == y && !s->getDead())
+            return i;
+    }
+    return -1;
+}
+
+bool SceneGame::getClickPos(CCPoint ptInWin, int &x, int &y)
+{
+    for(x=0;x<=8;x++)
+        for(y=0;y<=9;y++)
+        {
+            CCPoint ptInPlate = ccp(x*_d,y*_d) + _stoneOffset;
+            if(ptInWin.getDistance(ptInPlate) < _d/2)
+                return true;
+        }
+    return false;
 }
--- Classes/SceneGame.h
+++ Classes/SceneGame.h
@@ -5,10 +5,37 @@
 USING_NS_CC;
 #include "Stone.h"
 
+class Step : public CCObject
+{
+public:
+    int _moveid;
+    int _killid;
+    int _xFrom;
+    int _yFrom;
+    int _xTo;
+    int _yTo;
+    static Step* create(int moveid, int killid, int xFrom, int yFrom, int xTo, int yTo)
+    {
+        Step* step = new Step();
+        step->_moveid = moveid;
+        step->_killid = killid;
+        step->_xFrom = xFrom;
+        step->_yFrom = yFrom;
+        step->_xTo = xTo;
+        step->_yTo = yTo;
+        step->autorelease();
+        return step;
+    }
+};
+
 class SceneGame : public CCLayer
 {
 public:
     SceneGame();
+    ~SceneGame()
+    {
+        _steps->release();
+    }
 
     CCSize winSize = CCDirector::sharedDirector()->getWinSize();
 
@@ -21,6 +48,20 @@ public:
     CCPoint _stoneOffset; //第一个棋子的显示坐标
     float _d;
     Stone* _s[32];
+
+    bool ccTouchBegan(CCTouch *pTouch, CCEvent*);
+    int _selectid = -1;
+    bool getClickPos(CCPoint ptInWin, int& x, int& y);
+    int getStone(int x, int y);
+    void setSelectID(int id);
+    CCSprite* _selectSprite;
+    void moveStone(int moveid, int killid, int x, int y);
+    CCPoint getStonePos(int x, int y);
+    bool _redTurn;
+    bool _redSide;
+
+    void back(CCObject*);
+    CCArray* _steps;
 };
 
 #endif // SCENEGAME_H
